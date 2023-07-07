79349f5fb5404b4a6313937514957a2cbadb40e0
Rachael Hindley <rachael.hindley52@gmail.com>
2023-07-07 10:07:05 +0100

    added site pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
-import "./App.css";
+import "./Components/CSS/reset.css";
+// import "./App.css";
 import MyList from "./Components/MyList";
+import Header from "./Components/Header";
+import CreateList from "./Components/CreateList";
+import Inspiration from "./Components/Inspiration";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
-    return "Hi";
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Header />
+                <Routes>
+                    <Route path="/mylist" element={<MyList />} />
+                    <Route path="/createlist" element={<CreateList />} />{" "}
+                    <Route path="/inspiration" element={<Inspiration />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
--- src/Components/MyList.jsx
+++ src/Components/MyList.jsx
@@ -2,11 +2,25 @@ import "./CSS/MyList.css";
 
 export default function MyList() {
     return (
-        <form className="new-item-form">
-            <div className="form-row">
-                <label htmlFor="item">New Item</label>
-                <input type="text" id="item"></input>
-            </div>
-        </form>
+        <>
+            <form className="new-item-form">
+                <div className="form-row">
+                    <label htmlFor="item">New Item</label>
+                    <input type="text" id="item"></input>
+                </div>
+                <button className="btn btn-add">Add</button>
+            </form>
+            <h2 className="header">To-do List</h2>
+            <ul className="list">
+                <li>
+                    <label>
+                        <input type="checkbox" />
+                        Item 1
+                    </label>
+                    <button className="btn btn-edit">Edit</button>
+                    <button className="btn btn-delete">Delete</button>
+                </li>
+            </ul>
+        </>
     );
 }
